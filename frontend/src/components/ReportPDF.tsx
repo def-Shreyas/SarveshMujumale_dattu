87540490fb767a9baae6abdcb5de4a0216bf904b
def-Shreyas <shreyasmoolya2003@gmail.com>
2025-12-08 07:39:28 +0530

    v1.0 deployment version

--- frontend/src/components/ReportPDF.tsx
+++ frontend/src/components/ReportPDF.tsx
@@ -1,23 +1,23 @@
-import React, { useRef } from "react";
-import { usePDF } from "react-to-pdf";
+// import React, { useRef } from "react";
+// import { usePDF } from "react-to-pdf";
 
-export default function ReportPage() {
-  const { toPDF, targetRef } = usePDF({ filename: "Dattu_Report.pdf" });
+// export default function ReportPage() {
+//   const { toPDF, targetRef } = usePDF({ filename: "Dattu_Report.pdf" });
 
-  return (
-    <div>
-      <button
-        onClick={() => toPDF()}
-        className="px-4 py-2 bg-blue-600 text-white rounded"
-      >
-        Download PDF
-      </button>
+//   return (
+//     <div>
+//       <button
+//         onClick={() => toPDF()}
+//         className="px-4 py-2 bg-blue-600 text-white rounded"
+//       >
+//         Download PDF
+//       </button>
 
-      {/* The section that will be exported */}
-      <div ref={targetRef}>
-        {/** your dynamic HTML content goes here */}
-        <div dangerouslySetInnerHTML={{ __html: aiReport }} />
-      </div>
-    </div>
-  );
-}
+//       {/* The section that will be exported */}
+//       <div ref={targetRef}>
+//         {/** your dynamic HTML content goes here */}
+//         <div dangerouslySetInnerHTML={{ __html: aiReport }} />
+//       </div>
+//     </div>
+//   );
+// }
